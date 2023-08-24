0f0a592fef1a1744ab7ceb1c04fa9e258049d59b
6-JDodard <82525633+6-JDodard@users.noreply.github.com>
2023-08-24 08:53:01 -0400

    Item Collection

---
+++ Assets/ItemCollector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int cherry = 0;
+
+    [SerializeField] private Text cherryText;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Cherry"))
+        {
+            Destroy(collision.gameObject);
+            cherry++;
+            cherryText.text = "Cherry: " + cherry;
+        }
+    }
+}
