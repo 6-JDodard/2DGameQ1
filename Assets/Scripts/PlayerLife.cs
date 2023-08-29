3397416683b77d89099181eb9e8d7749cb6263e6
6-JDodard <82525633+6-JDodard@users.noreply.github.com>
2023-08-29 08:12:51 -0400

    Added Death Animation, player cannot die yet

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+
+    }
+
+}
