e6c45ee7e92168b5e37d056c7046b0f6407e8ca2
6-JDodard <82525633+6-JDodard@users.noreply.github.com>
2023-09-12 08:13:51 -0400

    Added Audio Mixer, working on death fall

---
+++ Assets/Scripts/DeathFall.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathFall : MonoBehaviour
+{
+  [SerializeField] private Play deathScript;
+    void Update()
+    {
+        if(transform.postiion.y < -10)
+    }
+}
