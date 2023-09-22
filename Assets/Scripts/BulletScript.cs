902d3083de3edf5177e3b078bdcfc27f90722996
6-JDodard <82525633+6-JDodard@users.noreply.github.com>
2023-09-22 08:13:28 -0400

    Finally Fixed Enemy shooting projectiles

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -18,8 +18,25 @@ public class BulletScript : MonoBehaviour
       Vector3 direction = player.transform.position - transform.position;
       rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
 
-      //float rot = Mathf. Atan2(-direction.y, -direction.x) * Mathf. Rad2Deg;
-      //transform. rotation = Quaternion.Euler(0,0, rot + 90); 
+      float rot = Mathf. Atan2(-direction.y, -direction.x) * Mathf. Rad2Deg;
+      transform. rotation = Quaternion.Euler(0,0, rot + 90); 
     }
- 
+
+    void Update()
+  {
+      timer += Time.deltaTime;
+      if(timer > 3)
+      {
+        timer = 0;
+        Destroy(gameObject);
+      }
+  }
+   private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if(collision.gameObject.CompareTag("Player"))
+        {
+          Destroy(gameObject);
+        }
+    }
 }
